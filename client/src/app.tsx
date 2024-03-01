49c50a12dd33d1f2a80d72ae3098f45d0d8f819d
Anthony McLamb <adylanmclamb@yahoo.com>
2024-03-01 15:17:10 -0500

    add page loader

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,6 +1,17 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import PageRoutes from "./pages/page-routes";
+import PageLoader from "./components/loader/page-loader/page-loader";
 
 const App = () => {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return (
+      <div>
+        <PageLoader />
+      </div>
+    );
+  }
   return <PageRoutes />;
 };
 
