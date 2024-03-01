57cfdde5fde6d7680e2e0fb7a648fbb974df4ac3
Anthony McLamb <adylanmclamb@yahoo.com>
2024-03-01 17:11:41 -0500

    update entities

--- backend/src/data-model/entities/index.ts
+++ backend/src/data-model/entities/index.ts
@@ -1,8 +1,11 @@
 import { BoardColumn } from './board-column.entity';
 import { Board } from './board.entity';
+import { Company } from './company.entity';
+import { Icon } from './icon.entity';
+import { Job } from './job.entity';
 
-const entities = [Board, BoardColumn];
+const entities = [Board, BoardColumn, Company, Icon, Job];
 
-export { Board, BoardColumn };
+export { Board, BoardColumn, Company, Icon, Job };
 
 export default entities;
