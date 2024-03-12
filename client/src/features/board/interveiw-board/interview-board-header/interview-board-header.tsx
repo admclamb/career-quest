bba04ba4dd5cf85034b4897481961d362c8075db
Anthony McLamb <adylanmclamb@yahoo.com>
2024-03-12 14:49:18 -0400

    Update navbar

--- client/src/features/board/interveiw-board/interview-board-header/interview-board-header.tsx
+++ client/src/features/board/interveiw-board/interview-board-header/interview-board-header.tsx
@@ -1,4 +1,9 @@
 import { BoardModel } from "@/models/board-model";
+import InterviewBoardHeaderNav from "./interview-board-header-nav/interview-board-header-nav";
+import { Link } from "react-router-dom";
+import { buttonVariants } from "@/components/ui/button";
+import AuthLogoutButton from "@/features/auth/auth-logout-button/auth-logout-button";
+import ModeToggle from "@/features/theme/mode-toggle";
 
 type Props = {
   board: BoardModel;
@@ -6,10 +11,29 @@ type Props = {
 
 const InterviewBoardHeader = ({ board }: Props) => {
   return (
-    <div className="flex justify-between border-b p-3 h-12">
-      <ul className="flex gap-5">
+    <div className="flex justify-between items-center border-b p-3 h-12">
+      <ul className="flex items-center gap-5">
+        <li className="flex items-center">
+          <InterviewBoardHeaderNav />
+        </li>
         <li>
           <h3 className="font-semibold">{board.title}</h3>
+        </li>
+      </ul>
+      <ul className="flex items-center gap-3">
+        <li>
+          <Link
+            to="/dashboard"
+            className={buttonVariants({ variant: "ghost" })}
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <AuthLogoutButton variant="outline">Log out</AuthLogoutButton>
+        </li>
+        <li>
+          <ModeToggle />
         </li>
       </ul>
     </div>
--- client/src/layout/layout-sidebar/layout-sidebar.tsx
+++ client/src/layout/layout-sidebar/layout-sidebar.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from "react";
-import Sidebar from "./sidebar/sidebar";
 import InterviewBoardHeader from "@/features/board/interveiw-board/interview-board-header/interview-board-header";
 import { useInterviewBoard } from "@/features/board/interveiw-board/inerview-board-provider";
 
@@ -12,7 +11,7 @@ const LayoutSidebar = ({ children, mainClassName = "" }: Props) => {
   const { board } = useInterviewBoard();
   return (
     <main className={`h-screen flex items-stretch ${mainClassName}`}>
-      <Sidebar />
+      {/* <Sidebar /> */}
       <div className="grow overflow-scroll flex flex-col">
         {board ? <InterviewBoardHeader board={board} /> : null}
 
